b3e60afa4381bcdaf306766888373e4a2fba18b0
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2026-01-22 07:41:41 -0600

    adds another endpoint for the server

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
-import logging
+import logging, datetime
+
+def time():
+    return datetime.datetime.now()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -14,11 +17,19 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+info = []
+
 @app.route("/data", methods=["POST"])
 def receive_data():
     data = request.json
+    info.append({"data": data, "timestamp": time()})
     logger.info("Received:", data)
     return jsonify({"status": "ok"})
 
+@app.route("/")
+def index():
+    return jsonify({"data": info})
+    
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
