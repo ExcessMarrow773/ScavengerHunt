44fbeb06f50d77df38f7bbb942b4b9cb9480f5cf
Atticus Walton <atticus.falkner.walton@gmail.com>
2026-03-04 20:57:51 +0000

    starts adding request models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class requests(models.Model):
+	device = models.CharField(max_length=50)
+	timestamp = models.TimeField(auto_now=True)
+	message = models.TextField()
+
+	def __str__(self):
+		return self.device
