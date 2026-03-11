fb3e5e65c504539dce394915d625b3a502b81d79
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2026-03-11 07:13:45 -0500

    adds accounts application

---
+++ accounts/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+	readonly_fields = ('followers_display', 'follower_count')
+	
+	fieldsets = (
+		(None, {
+			'fields': ('username', 'password')
+		}),
+		('Personal info', {
+			'fields': ('first_name', 'last_name', 'email')
+		}),
+		('Permissions', {
+			'fields': (
+				'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'
+			)
+		}),
+		('Important dates', {
+			'fields': ('last_login', 'date_joined')
+		}),
+	)	
+
+admin.site.register(CustomUser, CustomUserAdmin)
